2fa4a7a30642024289703e44592ec68bf961af47
werner <werner@41e4e6bf-1111-0410-9f04-deecbdac5fd9>
2007-03-22 12:46:05 +0000

    Added comments
    git-svn-id: file:///home/tclose/Desktop/neurofitter/Neurofitter@164 41e4e6bf-1111-0410-9f04-deecbdac5fd9

--- EOFitnessCalculator.h
+++ EOFitnessCalculator.h
@@ -18,19 +18,22 @@ using namespace std;
 	template< class EOT, class FitT = typename EOT::Fitness, class FunctionArg = const EOT& >
 #endif
 
+///A structure used by EO to represent the fitness function
 struct EOFitness: public eoEvalFunc<EOT> {
 
+	///Constructor reading the fitness evaluation function and the fitnesscalculator object
 	EOFitness( FitT (* _eval)( FunctionArg, FitnessCalculator * ), FitnessCalculator * f)
 		: eoEvalFunc<EOT>(), evalFunc( _eval ), fitness(f) {};
 
+	///Operator used to evaluate fitness function
 	virtual void operator() ( EOT & _eo ) {
 		if (_eo.invalid())
 			_eo.fitness((*evalFunc)( _eo, fitness ));
 		};
   
 	private:
-		FitT (* evalFunc )( FunctionArg, FitnessCalculator* );
-		FitnessCalculator * fitness;
+		FitT (* evalFunc )( FunctionArg, FitnessCalculator* ); ///< Evaluation function used by operator()
+		FitnessCalculator * fitness; ///< Fitness calculator object
 	}; 
 
 #endif
