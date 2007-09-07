5bec46e28ec0bb25db2332db9fd8bcd7cfe76807
ivan <ivan@41e4e6bf-1111-0410-9f04-deecbdac5fd9>
2007-09-07 02:07:55 +0000

    Corrected path to proccgi.sh invocation.
    git-svn-id: file:///home/tclose/Desktop/neurofitter/Neurofitter@210 41e4e6bf-1111-0410-9f04-deecbdac5fd9

--- scripts/websetup.sh
+++ scripts/websetup.sh
@@ -1,89 +1,93 @@
 #!/bin/sh
 
+eval `./proccgi.sh $* `
+
+
 echo Content-type: text/xml
 echo ""
 
 echo "<?xml version=\"1.0\"?>"
 
+
 /bin/cat <<EOF
 <TestProgram>
-	<Dimensions>${Dimensions}</Dimensions>
-	<VerboseLevel>${VerboseLevel}</VerboseLevel>
-	<Seed>${Seed}</Seed>
+	<Dimensions>${FORM_Dimensions}</Dimensions>
+	<VerboseLevel>${FORM_VerboseLevel}</VerboseLevel>
+	<Seed>${FORM_Seed}</Seed>
 	 
-	<SamplingFrequency>${SamplingFrequency}</SamplingFrequency> 
-	<StartingPoints>${StartingPoints}</StartingPoints> 
+	<SamplingFrequency>${FORM_SamplingFrequency}</SamplingFrequency> 
+	<StartingPoints>${FORM_StartingPoints}</StartingPoints> 
         <Bounds> <!-- LowerBound UpperBound --> 
-                        ${Bounds}
+                        ${FORM_Bounds}
         </Bounds> 
-        <WorkingDirectory>${WorkingDirectory}</WorkingDirectory> 
-        <PrintParameterFile>${PrintParameterFile}</PrintParameterFile> 
-        <FitterType>${FitterType}</FitterType> 
+        <WorkingDirectory>${FORM_WorkingDirectory}</WorkingDirectory> 
+        <PrintParameterFile>${FORM_PrintParameterFile}</PrintParameterFile> 
+        <FitterType>${FORM_FitterType}</FitterType> 
         <FitterParameters> 
-          <NumberOfPoints>${FitterParameters_NumberOfPoints}</NumberOfPoints> 
+          <NumberOfPoints>${FORM_FitterParameters_NumberOfPoints}</NumberOfPoints> 
         </FitterParameters> 
  
-        <TracesReaderType>${TracesReaderType}</TracesReaderType> 
+        <TracesReaderType>${FORM_TracesReaderType}</TracesReaderType> 
         
         <TracesReaderParameters> 
-	  <NumberOfRuns>${TracesReaderParameters_NumberOfRuns}</NumberOfRuns> 
-	  <NumberOfRunParameters>${TracesReaderParameters_NumberOfRunParameters}</NumberOfRunParameters> 
+	  <NumberOfRuns>${FORM_TracesReaderParameters_NumberOfRuns}</NumberOfRuns> 
+	  <NumberOfRunParameters>${FORM_TracesReaderParameters_NumberOfRunParameters}</NumberOfRunParameters> 
 	  <RunParameters> 
-		${TracesReaderParameters_RunParameters}
+		${FORM_TracesReaderParameters_RunParameters}
 	  </RunParameters> 
-	  <NumberOfPeriods>${TracesReaderParameters_NumberOfPeriods}</NumberOfPeriods> 
+	  <NumberOfPeriods>${FORM_TracesReaderParameters_NumberOfPeriods}</NumberOfPeriods> 
 	  <Periods> 
 	    <!-- PeriodStart PeriodSabs Weight --> 
-		    ${TracesReaderParameters_Periods}
+		    ${FORM_TracesReaderParameters_Periods}
 	  </Periods> 
 	 
-          <NumberOfRecordSites>${TracesReaderParameters_NumberOfRecordSites}</NumberOfRecordSites> 
+          <NumberOfRecordSites>${FORM_TracesReaderParameters_NumberOfRecordSites}</NumberOfRecordSites> 
 	  <RecordSites> 
 	        <!-- Weights --> 
-		${TracesReaderParameters_RecordSites} 
+		${FORM_TracesReaderParameters_RecordSites} 
 	  </RecordSites> 
-	  <OutputFilePrefix>${TracesReaderParameters_OutputFilePrefix}</OutputFilePrefix> 
+	  <OutputFilePrefix>${FORM_TracesReaderParameters_OutputFilePrefix}</OutputFilePrefix> 
         </TracesReaderParameters> 
 
-        <ModelType>${ModelType}</ModelType> 
+        <ModelType>${FORM_ModelType}</ModelType> 
         <ModelParameters> 
-	   <GenesisLocation>${ModelParameters_GenesisLocation}</GenesisLocation> 
+	   <GenesisLocation>${FORM_ModelParameters_GenesisLocation}</GenesisLocation> 
  
-	   <ModelDirectory>${ModelParameters_ModelDirectory}</ModelDirectory> 
-	   <OutputDirectory>${ModelParameters_OutputDirectory}</OutputDirectory> 
-	   <ModelSource>${ModelParameters_ModelSource}</ModelSource> 
-	   <ParameterFile>${ModelParameters_ParameterFile}</ParameterFile> 
+	   <ModelDirectory>${FORM_ModelParameters_ModelDirectory}</ModelDirectory> 
+	   <OutputDirectory>${FORM_ModelParameters_OutputDirectory}</OutputDirectory> 
+	   <ModelSource>${FORM_ModelParameters_ModelSource}</ModelSource> 
+	   <ParameterFile>${FORM_ModelParameters_ParameterFile}</ParameterFile> 
 	 
-	   <ShowExecuteOutput>${ModelParameters_ShowExecuteOutput}</ShowExecuteOutput>  
+	   <ShowExecuteOutput>${FORM_ModelParameters_ShowExecuteOutput}</ShowExecuteOutput>  
         </ModelParameters>  	
 	 
-        <ExperimentType>${ExperimentType}</ExperimentType> 
+        <ExperimentType>${FORM_ExperimentType}</ExperimentType> 
         <ExperimentParameters> 
-	   <Parameters>${ExperimentParameters}</Parameters>	 
+	   <Parameters>${FORM_ExperimentParameters}</Parameters>	 
         </ExperimentParameters> 
 	 
         <FitnessCalculatorType>Matrix</FitnessCalculatorType> 
         <FitnessCalculatorParameters> 
   	  <VdVdtMatrixType>Direct</VdVdtMatrixType> 
 	  <VdVdtMatrixParameters> 
-	    <vLength>${VdVdtMatrixParameters_vLength}</vLength> 
-	    <dVdtLength>${VdVdtMatrixParameters_dVdtLength}</dVdtLength> 
-	    <minimalV>${VdVdtMatrixParameters_minimalV}</minimalV> 
+	    <vLength>${FORM_VdVdtMatrixParameters_vLength}</vLength> 
+	    <dVdtLength>${FORM_VdVdtMatrixParameters_dVdtLength}</dVdtLength> 
+	    <minimalV>${FORM_VdVdtMatrixParameters_minimalV}</minimalV> 
 	    <maximalV>0.05</maximalV> 
                    <comparePrecision>1e-15</comparePrecision> 
 	    <numericOutputFormat>0</numericOutputFormat> 
 	    <SumOfSquareRoots>1</SumOfSquareRoots> 
 	</VdVdtMatrixParameters> 
-	<enableFileExport>${FitnessCalculatorParameters_enableFileExport}</enableFileExport> 
-	<exportFile>${FitnessCalculatorParameters_exportFile}</exportFile> 
+	<enableFileExport>${FORM_FitnessCalculatorParameters_enableFileExport}</enableFileExport> 
+	<exportFile>${FORM_FitnessCalculatorParameters_exportFile}</exportFile> 
       </FitnessCalculatorParameters> 
 EOF
 
-case "${FitterType}" in
+case "${FORM_FitterType}" in
 	Random) /bin/cat <<EOF
 	    <FitterType>Random</FitterType>
 	    <FitterParameters> 
-	        <NumberOfPoints>${Random_NumberOfPoints}</NumberOfPoints> 
+	        <NumberOfPoints>${FORM_Random_NumberOfPoints}</NumberOfPoints> 
 	    </FitterParameters>
 EOF
 	break ;;
@@ -91,7 +95,7 @@ EOF
 	Swarm) /bin/cat <<EOF
 	    <FitterType>Swarm</FitterType> 
 	    <FitterParameters> 
-	        <NumberOfPoints>${Swarm_NumberOfRuns}</NumberOfPoints> 
+	        <NumberOfPoints>${FORM_Swarm_NumberOfRuns}</NumberOfPoints> 
 	    </FitterParameters>
 EOF
 	break ;;
@@ -99,48 +103,48 @@ EOF
 	NOMAD) /bin/cat <<EOF
 	    <FitterType>NOMAD</FitterType> 
 	    <FitterParameters> 
-	       <OutputFile>${NOMAD_OutputFile}</OutputFile> 
+	       <OutputFile>${FORM_NOMAD_OutputFile}</OutputFile> 
 	    <!-- parameters.dat --> 
-	      <COARSENING_EXPONENT>   ${NOMAD_COARSENING_EXPONENT}</COARSENING_EXPONENT> <!-- Exponent to enlarge poll size in case of failure --> 
-	      <INITIAL_POLL_SIZE>     ${NOMAD_INITIAL_POLL_SIZE}</INITIAL_POLL_SIZE> 
-	      <POLL_BASIS>            ${NOMAD_POLL_BASIS}</POLL_BASIS> 
-	      <REFINING_EXPONENT>     ${NOMAD_REFINING_EXPONENT}</REFINING_EXPONENT> 
-	      <MAX_POLL_SIZE>         ${NOMAD_MAX_POLL_SIZE}</MAX_POLL_SIZE> 
+	      <COARSENING_EXPONENT>   ${FORM_NOMAD_COARSENING_EXPONENT}</COARSENING_EXPONENT> <!-- Exponent to enlarge poll size in case of failure --> 
+	      <INITIAL_POLL_SIZE>     ${FORM_NOMAD_INITIAL_POLL_SIZE}</INITIAL_POLL_SIZE> 
+	      <POLL_BASIS>            ${FORM_NOMAD_POLL_BASIS}</POLL_BASIS> 
+	      <REFINING_EXPONENT>     ${FORM_NOMAD_REFINING_EXPONENT}</REFINING_EXPONENT> 
+	      <MAX_POLL_SIZE>         ${FORM_NOMAD_MAX_POLL_SIZE}</MAX_POLL_SIZE> 
 	      
-	      <POLL_ORDER>            ${NOMAD_POLL_ORDER}</POLL_ORDER> <!-- Dynamic 0/1 --> 
-	      <POLL_COMPLETE>         ${NOMAD_POLL_COMPLETE}</POLL_COMPLETE> <!-- Opportunistic 0/1 --> 
-	      <POLL_DIRECTIONS>       ${NOMAD_POLL_DIRECTIONS}</POLL_DIRECTIONS> <!-- MADS directions 0/1/2/3/4--> 
+	      <POLL_ORDER>            ${FORM_NOMAD_POLL_ORDER}</POLL_ORDER> <!-- Dynamic 0/1 --> 
+	      <POLL_COMPLETE>         ${FORM_NOMAD_POLL_COMPLETE}</POLL_COMPLETE> <!-- Opportunistic 0/1 --> 
+	      <POLL_DIRECTIONS>       ${FORM_NOMAD_POLL_DIRECTIONS}</POLL_DIRECTIONS> <!-- MADS directions 0/1/2/3/4--> 
 	      
-	      <INITIAL_SEARCH>        ${NOMAD_INITIAL_SEARCH}</INITIAL_SEARCH> <!-- No initial search 0/1/3 --> 
-	      <INITIAL_COMPLETE>      ${NOMAD_INITIAL_COMPLETE}</INITIAL_COMPLETE> <!-- Opportunistic 0/1 --> 
-	      <INITIAL_POINTS>        ${NOMAD_INITIAL_POINTS}</INITIAL_POINTS> <!-- Number of points after initial search --> 
+	      <INITIAL_SEARCH>        ${FORM_NOMAD_INITIAL_SEARCH}</INITIAL_SEARCH> <!-- No initial search 0/1/3 --> 
+	      <INITIAL_COMPLETE>      ${FORM_NOMAD_INITIAL_COMPLETE}</INITIAL_COMPLETE> <!-- Opportunistic 0/1 --> 
+	      <INITIAL_POINTS>        ${FORM_NOMAD_INITIAL_POINTS}</INITIAL_POINTS> <!-- Number of points after initial search --> 
 	      
-	      <ITERATIVE_SEARCH>      ${NOMAD_ITERATIVE_SEARCH}</ITERATIVE_SEARCH> <!-- Random search at start every iteration 0/1/2 --> 
-	      <ITERATIVE_COMPLETE>    ${NOMAD_ITERATIVE_COMPLETE}</ITERATIVE_COMPLETE> <!-- Opportunistic 0/1 --> 
-	      <ITERATIVE_POINTS>      ${NOMAD_ITERATIVE_POINTS}</ITERATIVE_POINTS> <!-- Number of points generated by iteration --> 
+	      <ITERATIVE_SEARCH>      ${FORM_NOMAD_ITERATIVE_SEARCH}</ITERATIVE_SEARCH> <!-- Random search at start every iteration 0/1/2 --> 
+	      <ITERATIVE_COMPLETE>    ${FORM_NOMAD_ITERATIVE_COMPLETE}</ITERATIVE_COMPLETE> <!-- Opportunistic 0/1 --> 
+	      <ITERATIVE_POINTS>      ${FORM_NOMAD_ITERATIVE_POINTS}</ITERATIVE_POINTS> <!-- Number of points generated by iteration --> 
 	      
-              <SPECULATIVE_SEARCH>    ${NOMAD_SPECULATIVE_SEARCH}</SPECULATIVE_SEARCH> <!-- Do a search in poll direction (only MADS) 0/1 --> 
+              <SPECULATIVE_SEARCH>    ${FORM_NOMAD_SPECULATIVE_SEARCH}</SPECULATIVE_SEARCH> <!-- Do a search in poll direction (only MADS) 0/1 --> 
 	      
-	      <POLL_SIZE_TERM>        ${NOMAD_POLL_SIZE_TERM}</POLL_SIZE_TERM> <!-- Terminate when poll size is smaller than this value --> 
-	      <ITERATIONS>            ${NOMAD_ITERATIONS}</ITERATIONS> <!-- Max number of iterations --> 
-	      <TRUTH_EVALS>           ${NOMAD_TRUTH_EVALS}</TRUTH_EVALS> <!-- Max number of truth evaluations --> 
-	      <NEW_TRUTH_EVALS>       ${NOMAD_NEW_TRUTH_EVALS}</NEW_TRUTH_EVALS> <!-- Max number of non-cache truth evaluations --> 
-	      <CONSECUTIVE_FAILS>     ${NOMAD_CONSECUTIVE_FAILS}</CONSECUTIVE_FAILS> <!-- Max number of consecutive better-solution-failures --> 
-	      <FILTER_RELATIVE>       ${NOMAD_FILTER_RELATIVE}</FILTER_RELATIVE> <!-- HMax is relative to starting point 0/1 --> 
-	      <HMAX>                  ${NOMAD_HMAX}</HMAX> 
-	      <HMIN>                  ${NOMAD_HMIN}</HMIN>      
-	      <FILTER_NORM>           ${NOMAD_FILTER_NORM}</FILTER_NORM> <!-- L-2 squared norm 0/1/2 --> 
+	      <POLL_SIZE_TERM>        ${FORM_NOMAD_POLL_SIZE_TERM}</POLL_SIZE_TERM> <!-- Terminate when poll size is smaller than this value --> 
+	      <ITERATIONS>            ${FORM_NOMAD_ITERATIONS}</ITERATIONS> <!-- Max number of iterations --> 
+	      <TRUTH_EVALS>           ${FORM_NOMAD_TRUTH_EVALS}</TRUTH_EVALS> <!-- Max number of truth evaluations --> 
+	      <NEW_TRUTH_EVALS>       ${FORM_NOMAD_NEW_TRUTH_EVALS}</NEW_TRUTH_EVALS> <!-- Max number of non-cache truth evaluations --> 
+	      <CONSECUTIVE_FAILS>     ${FORM_NOMAD_CONSECUTIVE_FAILS}</CONSECUTIVE_FAILS> <!-- Max number of consecutive better-solution-failures --> 
+	      <FILTER_RELATIVE>       ${FORM_NOMAD_FILTER_RELATIVE}</FILTER_RELATIVE> <!-- HMax is relative to starting point 0/1 --> 
+	      <HMAX>                  ${FORM_NOMAD_HMAX}</HMAX> 
+	      <HMIN>                  ${FORM_NOMAD_HMIN}</HMIN>      
+	      <FILTER_NORM>           ${FORM_NOMAD_FILTER_NORM}</FILTER_NORM> <!-- L-2 squared norm 0/1/2 --> 
 	      
-	      <SURROGATE_TOLERANCE>   ${NOMAD_SURROGATE_TOLERANCE}</SURROGATE_TOLERANCE> 
+	      <SURROGATE_TOLERANCE>   ${FORM_NOMAD_SURROGATE_TOLERANCE}</SURROGATE_TOLERANCE> 
 	      
 	      <!-- description.dat --> 
-	      <SCALING_CHOICE>        ${NOMAD_SCALING_CHOICE}</SCALING_CHOICE> <!-- No scaling 0/1/2/3 --> 
-	      <GEN_CONS_NB>           ${NOMAD_GEN_CONS_NB}</GEN_CONS_NB> <!-- Number of constraints --> 
-	      <USE_SURROGATE>         ${NOMAD_USE_SURROGATE}</USE_SURROGATE> <!-- Use surrogate 0/1 --> 
+	      <SCALING_CHOICE>        ${FORM_NOMAD_SCALING_CHOICE}</SCALING_CHOICE> <!-- No scaling 0/1/2/3 --> 
+	      <GEN_CONS_NB>           ${FORM_NOMAD_GEN_CONS_NB}</GEN_CONS_NB> <!-- Number of constraints --> 
+	      <USE_SURROGATE>         ${FORM_NOMAD_USE_SURROGATE}</USE_SURROGATE> <!-- Use surrogate 0/1 --> 
 	      
-	      <START_PT_FILE>         ${NOMAD_START_PT_FILE}</START_PT_FILE> 
-	      <BOUNDS_FILE>           ${NOMAD_BOUNDS_FILE}</BOUNDS_FILE> 
-	      <RESULTS_FILE>          ${NOMAD_RESULTS_FILE}</RESULTS_FILE> 
+	      <START_PT_FILE>         ${FORM_NOMAD_START_PT_FILE}</START_PT_FILE> 
+	      <BOUNDS_FILE>           ${FORM_NOMAD_BOUNDS_FILE}</BOUNDS_FILE> 
+	      <RESULTS_FILE>          ${FORM_NOMAD_RESULTS_FILE}</RESULTS_FILE> 
 	    </FitterParameters>
 EOF
 	break ;;
@@ -148,19 +152,19 @@ EOF
 	EO) /bin/cat <<EOF
 	    <FitterType>EO</FitterType> 
 	    <FitterParameters> 
-	      <popSize>      ${EO_popSize}</popSize> 
-	      <nbOffspring>  ${EO_nbOffspring}</nbOffspring> 
-	      <replacement>  ${EO_replacement}</replacement> 
-	      <maxGen>       ${EO_maxGen}</maxGen> 
-	      <minGen>       ${EO_minGen}</minGen> 
-	      <maxEval>      ${EO_maxEval}</maxEval> 
-	      <targetFitness>${EO_targetFitness}</targetFitness> 
-	      <steadyGen>    ${EO_steadyGen}</steadyGen> 
-	      <crossType>    ${EO_crossType}</crossType> 
-	      <crossObj>     ${EO_crossObj}</crossObj>  
-	      <TauLoc>       ${EO_TauLoc}</TauLoc> 
-	      <TauGlob>      ${EO_TauGlob}</TauGlob> 
-	      <Beta>         ${EO_Beta}</Beta> 
+	      <popSize>      ${FORM_EO_popSize}</popSize> 
+	      <nbOffspring>  ${FORM_EO_nbOffspring}</nbOffspring> 
+	      <replacement>  ${FORM_EO_replacement}</replacement> 
+	      <maxGen>       ${FORM_EO_maxGen}</maxGen> 
+	      <minGen>       ${FORM_EO_minGen}</minGen> 
+	      <maxEval>      ${FORM_EO_maxEval}</maxEval> 
+	      <targetFitness>${FORM_EO_targetFitness}</targetFitness> 
+	      <steadyGen>    ${FORM_EO_steadyGen}</steadyGen> 
+	      <crossType>    ${FORM_EO_crossType}</crossType> 
+	      <crossObj>     ${FORM_EO_crossObj}</crossObj>  
+	      <TauLoc>       ${FORM_EO_TauLoc}</TauLoc> 
+	      <TauGlob>      ${FORM_EO_TauGlob}</TauGlob> 
+	      <Beta>         ${FORM_EO_Beta}</Beta> 
 	    </FitterParameters> 
 EOF
 	break ;;
